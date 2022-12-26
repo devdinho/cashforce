1f5071678b3b96ad802e3e2ea19d4e7452dd723c
Diego Figueirêdo <8kball@gmail.com>
2022-12-26 19:59:56 -0300

    chore: fetching data from server through the client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
+import { useEffect } from 'react';
+
+const getResponseFromServer = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/");
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+  catch(err) {
+    console.log(err);
+  }
+}
 
 function App() {
+  useEffect(() => {
+    getResponseFromServer();
+  }, []);
+
   return (
     <>
       <Sidebar />
