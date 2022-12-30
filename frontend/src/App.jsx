927e3fa08bf375d300b43b1453adbefedc80f112
Diego Figueirêdo <8kball@gmail.com>
2022-12-30 13:45:23 -0300

    refactor: got rid of useEffect and backend call from the main component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,8 @@
 import './App.css'
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
-import { useEffect } from 'react';
-
-const getResponseFromServer = async () => {
-  try {
-    const response = await fetch("http://localhost:3000/");
-    const data = await response.json();
-    console.log(data);
-    return data;
-  }
-  catch(err) {
-    console.log(err);
-  }
-}
 
 function App() {
-  useEffect(() => {
-    getResponseFromServer();
-  }, []);
-
   return (
     <>
       <Sidebar />
@@ -28,4 +11,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
