881090afe784a5b1dcd0e68b8ce6c4259823544a
Diego Figueirêdo <8kball@gmail.com>
2022-12-30 12:07:41 -0300

    build: add Buyer model

---
+++ backend/src/database/models/Buyer.js
@@ -0,0 +1,84 @@
+const { DataTypes } = require("sequelize");
+const database = require("../database");
+
+const Buyer = database.define("buyer", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  tradingName: {
+    type: DataTypes.STRING,
+  },
+  cashforceTax: {
+    type: DataTypes.STRING,
+  },
+  responsibleName: {
+    type: DataTypes.STRING,
+  },
+  responsibleEmail: {
+    type: DataTypes.STRING,
+  },
+  responsiblePosition: {
+    type: DataTypes.STRING,
+  },
+  responsiblePhone: {
+    type: DataTypes.STRING,
+  },
+  responsibleMobile: {
+    type: DataTypes.STRING,
+  },
+  website: {
+    type: DataTypes.STRING,
+  },
+  postalCode: {
+    type: DataTypes.STRING,
+  },
+  address: {
+    type: DataTypes.STRING,
+  },
+  number: {
+    type: DataTypes.STRING,
+  },
+  complement: {
+    type: DataTypes.STRING,
+  },
+  neighborhood: {
+    type: DataTypes.STRING,
+  },
+  city: {
+    type: DataTypes.STRING,
+  },
+  state: {
+    type: DataTypes.STRING,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+  },
+  situation: {
+    type: DataTypes.STRING,
+  },
+  situationDate: {
+    type: DataTypes.STRING,
+  },
+  cnpjId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "cnpjs",
+      key: "id",
+    }
+  },
+  confirm: {
+    type: DataTypes.TINYINT,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = Buyer;
