5600b2ce6c6939b12f04da1ac56da6cc0832812d
Diego Figueirêdo <8kball@gmail.com>
2022-12-30 13:43:50 -0300

    refactor: imports added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const router = require('./routes');
+const database = require("./database/database");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World!" });
-});
+app.use("/api", router);
 
-app.listen(3000, () => console.log("Server running..."));
+app.listen(3000, async () => {
+  await database.sync();
+  console.log("Server running...");
+});
