ee8258e1fe98b34accbf51fdc9ddc0962aac09c3
Diego Figueirêdo <8kball@gmail.com>
2022-12-30 12:08:18 -0300

    build: add Cnpj model

---
+++ backend/src/database/models/Cnpj.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+const database = require("../database");
+
+const Cnpj = database.define("cnpj", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  cnpj: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  companyType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+
+module.exports = Cnpj;
